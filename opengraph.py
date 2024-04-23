7d8eeb261f8a04a063661fc643c81132006ca276
Rik Van Moll <rikvnmll@gmail.com>
2024-04-23 11:05:10 +0200

    Finished part 1

--- opengraph.py
+++ opengraph.py
@@ -9,9 +9,15 @@ def fetch_metadata(url):
     """
     Returns the "data" dictionary of OpenGraph metadata found in HTML of given url
     """
-    pass  # YOUR CODE HERE
+    response = requests.get("https://opengraph.lewagon.com", params = {"url":url})
+    if response.status_code != 200:
+        return {}
+    return response.json()["data"]
 
-# To manually test, please uncomment the following lines and run `python opengraph.py`:
+
+
+
+#To manually test, please uncomment the following lines and run `python opengraph.py`:
 # import pprint
 # pp = pprint.PrettyPrinter(indent=4)
 # pp.pprint(fetch_metadata("https://www.github.com"))
